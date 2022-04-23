a190720a2613dc5d0326c4268365454030d8f6a8
Журавлев П.В <pv.zhuravlev@tensor.ru>
2022-04-23 22:00:25 +0300

    rollback на апдейт

--- db.py
+++ db.py
@@ -150,12 +150,16 @@ async def lvl_change(user_id, lvl):
     """Изменение уровня"""
 
     sql = """
+        BEGIN;
+        SAVEPOINT lvl_savepoint;
         UPDATE
             "Users"
         SET
             "Level" = "Level" + :lvl
         WHERE
-            "UserID" = :user_id
+            "UserID" = :user_id;
+        ROLLBACK TO SAVEPOINT lvl_savepoint;
+        COMMIT;
     """
 
     await database.execute(sql, values={'user_id': user_id, 'lvl': lvl})
