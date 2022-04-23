0b603590be9323ac244aaee1443233774a87ed70
Журавлев П.В <pv.zhuravlev@tensor.ru>
2022-04-23 18:10:43 +0300

    init commit

---
+++ app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+
+import config
+
+
+bot = Bot(token=config.TOKEN)
+dp = Dispatcher(bot)
+
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(config.WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=config.WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=config.WEBAPP_HOST,
+        port=config.WEBAPP_PORT,
+    )
